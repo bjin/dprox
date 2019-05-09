27daaf5538d37c87276753fd88f260b3de97b153
Bin Jin <bjin@ctrl-d.org>
2019-05-09 16:10:25 +0800

    add license header

--- DomainRoute.hs
+++ DomainRoute.hs
@@ -1,3 +1,6 @@
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Copyright (C) 2019 Bin Jin. All Rights Reserved.
 {-# LANGUAGE RecordWildCards #-}
 module DomainRoute
 ( DomainRoute
--- Test.hs
+++ Test.hs
@@ -1,3 +1,6 @@
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Copyright (C) 2019 Bin Jin. All Rights Reserved.
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
