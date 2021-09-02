30950f57b4f60c871f81040d3bc689afe1a37569
Bin Jin <bjin@ctrl-d.org>
2021-09-02 18:15:33 +0800

    DomainRoute: use bytestring-trie

--- src/DomainRoute.hs
+++ src/DomainRoute.hs
@@ -1,7 +1,7 @@
 -- SPDX-License-Identifier: BSD-3-Clause
 --
 -- Copyright (C) 2019 Bin Jin. All Rights Reserved.
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module DomainRoute
 ( DomainRoute
 , newDomainRoute
@@ -9,69 +9,29 @@ module DomainRoute
 , getDomainRouteByPrefix
 ) where
 
-import qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString.Short as SBS
-import           Data.Function         (on)
-import           Data.HashMap.Strict   (HashMap)
-import qualified Data.HashMap.Strict   as HashMap
-import           Data.List             (groupBy, sortOn, span)
+import qualified Data.ByteString.Char8 as BS
+import           Data.Char             (toLower)
+import qualified Data.Trie             as T
+import qualified Data.Trie.Convenience as T
 import qualified Network.DNS           as DNS
 
-type DomainComp = SBS.ShortByteString
+newtype DomainRoute a = DomainRoute (T.Trie a)
+    deriving (Eq, Show, Functor, Foldable)
 
-data DomainRoute a = DomainRoute
-    { currentNode :: Maybe a
-    , children    :: Maybe (HashMap DomainComp (DomainRoute a))
-    } deriving (Eq, Show)
-
-instance Functor DomainRoute where
-    fmap f DomainRoute{..} = DomainRoute (fmap f currentNode) ((fmap.fmap.fmap) f children)
-
-instance Foldable DomainRoute where
-    foldMap f DomainRoute{..} = foldMap f currentNode `mappend` (foldMap.foldMap.foldMap) f children
+normalize :: DNS.Domain -> BS.ByteString
+normalize d
+    | BS.null d = BS.empty
+    | otherwise = BS.reverse (BS.cons '.' (BS.map toLower d))
 
 newDomainRoute :: (a -> a -> a) -> [(DNS.Domain, a)] -> DomainRoute a
-newDomainRoute merge servers = go sortedServers
+newDomainRoute merge ds = DomainRoute (T.fromListWithL' merge ds')
   where
-    fmapFst f (x, y) = (f x, y)
-    sortedServers = sortOn fst $ map (fmapFst getDomainComponents) servers
-
-    go [] = DomainRoute Nothing Nothing
-    go lst = DomainRoute node childs
-      where
-        (prefix, suffix) = span (null . fst) lst
-        grouped = groupBy ((==) `on` (head.fst)) suffix
-
-        node = if null prefix then Nothing else Just (foldr1 merge (map snd prefix))
-        subtrees = [ (comp, go subtree)
-                   | child <- grouped
-                   , let comp = head (fst (head child))
-                   , let subtree = map (fmapFst tail) child
-                   ]
-        childs = if null subtrees then Nothing else Just (HashMap.fromList subtrees)
-
-queryDomainRoute :: Bool -> DomainRoute a -> DNS.Domain -> Maybe a
-queryDomainRoute allowPrefix dr domain = go dr comps
-  where
-    comps = getDomainComponents domain
-
-    placePrefix _ b       | not allowPrefix = b
-    placePrefix a Nothing = a
-    placePrefix _ b       = b
-
-    go DomainRoute{..} [] = currentNode
-    go (DomainRoute node Nothing) _ = placePrefix node Nothing
-    go (DomainRoute node (Just child)) (x:xs) = placePrefix node $ case HashMap.lookup x child of
-        Nothing      -> Nothing
-        Just subtree -> go subtree xs
+    ds' =  [(normalize domain, record) | (domain, record) <- ds]
 
 getDomainRouteExact :: DomainRoute a -> DNS.Domain -> Maybe a
-getDomainRouteExact = queryDomainRoute False
+getDomainRouteExact (DomainRoute trie) d = T.lookup (normalize d) trie
 
 getDomainRouteByPrefix :: DomainRoute a -> DNS.Domain -> Maybe a
-getDomainRouteByPrefix = queryDomainRoute True
-
-getDomainComponents :: DNS.Domain -> [DomainComp]
-getDomainComponents domain = reverse $ filter (not . SBS.null) comps
-  where
-    comps = map SBS.toShort $ BS8.split '.' (DNS.normalizeCase domain)
+getDomainRouteByPrefix (DomainRoute trie) d = case T.match trie (normalize d) of
+    Nothing          -> Nothing
+    Just (_, res, _) -> Just res
